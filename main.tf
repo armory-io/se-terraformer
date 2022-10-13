d7a773d6745a9dfb5141d6ea078e3c91c7204d43
bealesh <david@beale.sh>
2022-10-13 09:22:42 -0500

    update tf for vandalay

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 module "s3" {
     source = "./s3"
     # bucket name should be unique
-    bucket_name = "armory-terraformer-foobar"   
+    bucket_name = "armory-terraformer-vandaly"   
 }
